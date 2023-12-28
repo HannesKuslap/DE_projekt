ad9d17aea5ddaa35e4cfa38480638cedf3ee20d3
evalottalepp <evalepp@gmail.com>
2023-12-28 15:05:30 +0200

    Data cleansing + csv-converting function

--- data/transform.py
+++ data/transform.py
@@ -1,5 +1,6 @@
-import json
 import pandas as pd
+import json
+from datetime import datetime
 
 def read_json(line):
     one_article = json.loads(line)
@@ -12,22 +13,49 @@ def flatten(target):
         result.append(full_name.strip())
     return result
 
-# Reads a jsonfile, creates a csv 
-def transform_data(jsonfile, output_csv):
-    data = pd.DataFrame(columns=('id', 'authors', 'title', 'journal-ref', 'doi', 'categories', 'date_created'))
-    with open(jsonfile, "r", encoding='utf-8') as json_file:
-        n = 0
-        for line in json_file:
-            one_article = read_json(line)
-            if one_article['doi'] == None: # vb ei peaks ikka nii tegema aga praegu viskab välja kõik millel pole doi-d
-                continue
+def convert_to_iso_date(datetime_string):
+    if pd.notna(datetime_string):  # Check if the value is not None/NaN
+        try:
+            # Try to parse as '2009-06-23'
+            parsed_date = datetime.strptime(datetime_string, '%Y-%m-%d')
+        except ValueError:
+            # If parsing as '2009-06-23' fails, try the original format
+            parsed_date = datetime.strptime(datetime_string, "%a, %d %b %Y %H:%M:%S %Z")
 
-            authors = flatten(one_article['authors_parsed'])
-            # formaat ["perekonnanimi, eesnimi", "perekonnanimi, eesnimi", ...]
-            data.loc[n] = [one_article['id'], authors, one_article['title'],
-                        one_article['journal-ref'], one_article['doi'], one_article['categories'].split(" "), 
-                        one_article['versions'][0]['created']]
-            n += 1
-            #if n == 10000:
-                #return data
-    data.to_csv(output_csv, index=False)
+        return parsed_date.strftime('%Y-%m-%d')
+    else:
+        return None
+    
+def load_data(jsonfile):
+    with open(jsonfile, "r", encoding='utf-8') as json_file:
+        ids, titles, authors, journal_refs, categories, dois, creations, updates = [], [], [], [], [], [], [], []
+        
+        file = json.load(json_file)
+        n = 0
+        for one_article in file: 
+            ids.append(one_article['id'])
+            titles.append(one_article['title'])
+            authors.append(flatten(one_article['authors_parsed']))
+            journal_refs.append(one_article['journal-ref'])
+            # Leave only the first category and DOI code
+            categories.append(one_article['categories'].split(" ")[0])
+            dois.append(one_article['doi'].split(" ")[0])
+            # Unify the datetimes
+            creations.append(convert_to_iso_date(one_article['versions'][0]['created']))
+            updates.append(convert_to_iso_date(one_article['update_date']))
+            
+            n+=1
+
+    data = {'id': ids, 'authors': authors, 'title': titles, 'journal_ref': journal_refs, 'doi': dois, 'category_main': categories, 'date_created': creations, 'date_updated': updates}
+    return pd.DataFrame.from_dict(data)
+
+# Main function to load, wrangle and transform the data into a csv file. 
+def transform_data(jsonfile):
+    dataframe = load_data(jsonfile)
+
+    # Remove articles where title is only one word. 
+    short_title_mask = dataframe['title'].apply(lambda x: len(x.split()) > 1)
+    dataframe = dataframe[short_title_mask]
+
+    csv_file_path = jsonfile.split(".")[0] + '.csv'
+    dataframe.to_csv(csv_file_path, index=False)
